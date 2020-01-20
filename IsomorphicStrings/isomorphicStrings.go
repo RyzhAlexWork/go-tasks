eb98d1715e93cef798e73a03b9cf1d739a17be6c
Рыжов Алексей Денисович <ryzhov.aleksey4@wildberries.ru>
2020-01-20 10:36:54 +0300

    isomorphicStrings исправления

--- IsomorphicStrings/isomorphicStrings.go
+++ IsomorphicStrings/isomorphicStrings.go
@@ -1,8 +1,12 @@
-package IsomorphicStrings
+package isomorphicStrings
 
-func IsIsomorphic(s string, t string) bool {
+func isIsomorphic(s string, t string) bool {
+	//Создание хеш-таблицы под символы
 	dictionary := map[byte]byte{}
 	for i := 0; i < len(s); i++ {
+		//Если такого символа нет в таблице и нет такого значения, как в данном символе,
+		//то в хеш-таблицу записывается этот символ с данным значением. Иначе возвращается
+		//false
 		if _, ok := dictionary[s[i]]; !ok {
 			for _, value := range dictionary {
 				if value == t[i] {
@@ -11,6 +15,8 @@ func IsIsomorphic(s string, t string) bool {
 			}
 			dictionary[s[i]] = t[i]
 		} else {
+			//Если такой символ уже есть в таблице, но у него другое значение, возвращается
+			//false
 			if dictionary[s[i]] != t[i] {
 				return false
 			}
--- IsomorphicStrings/isomorphicStrings_test.go
+++ IsomorphicStrings/isomorphicStrings_test.go
@@ -1,87 +1,25 @@
-package IsomorphicStrings
+package isomorphicStrings
 
 import "testing"
 
-func TestIsIsomorphic_1(t *testing.T) {
-	str1 := "egg"
-	str2 := "add"
-
-	got := IsIsomorphic(str1, str2)
-	want := true
-
-	if want != got {
-		t.Errorf("got %v want %v given", got, want)
+var (
+	inputs = [][]string{
+		[]string{"egg", "add"},
+		[]string{"foo", "bar"},
+		[]string{"paper", "title"},
+		[]string{"ab", "aa"},
+		[]string{"aa", "ab"},
+		[]string{"aabbccaa", "ooppggoo"},
+		[]string{"aabbcccc", "ooppggoo"},
 	}
-}
+	wants = []bool{true, false, true, false, false, true, false}
+)
 
-func TestIsIsomorphic_2(t *testing.T) {
-	str1 := "foo"
-	str2 := "bar"
-
-	got := IsIsomorphic(str1, str2)
-	want := false
-
-	if want != got {
-		t.Errorf("got %v want %v given", got, want)
-	}
-}
-
-func TestIsIsomorphic_3(t *testing.T) {
-	str1 := "paper"
-	str2 := "title"
-
-	got := IsIsomorphic(str1, str2)
-	want := true
-
-	if want != got {
-		t.Errorf("got %v want %v given", got, want)
-	}
-}
-
-func TestIsIsomorphic_4(t *testing.T) {
-	str1 := "ab"
-	str2 := "aa"
-
-	got := IsIsomorphic(str1, str2)
-	want := false
-
-	if want != got {
-		t.Errorf("got %v want %v given", got, want)
-	}
-}
-
-func TestIsIsomorphic_5(t *testing.T) {
-	str1 := "aa"
-	str2 := "ab"
-
-	got := IsIsomorphic(str1, str2)
-	want := false
-
-	if want != got {
-		t.Errorf("got %v want %v given", got, want)
-	}
-}
-
-func TestIsIsomorphic_6(t *testing.T) {
-	str1 := "aabbccaa"
-	str2 := "ooppggoo"
-
-	got := IsIsomorphic(str1, str2)
-	want := true
-
-	if want != got {
-		t.Errorf("got %v want %v given", got, want)
-	}
-}
-
-func TestIsIsomorphic_7(t *testing.T) {
-	str1 := "aabbcccc"
-	str2 := "ooppggoo"
-
-	got := IsIsomorphic(str1, str2)
-	want := false
-
-	if want != got {
-		t.Errorf("got %v want %v given", got, want)
+func TestIsIsomorphic(t *testing.T) {
+	for i, want := range wants {
+		got := isIsomorphic(inputs[i][0], inputs[i][1])
+		if want != got {
+			t.Errorf("got: %v, want: %v, input_1: %v, input_2: %v", got, want, inputs[i][0], inputs[i][1])
+		}
 	}
 }
